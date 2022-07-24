add379e7104cb70dc2e5a0c113fb41b20ad1a07e
firas999 <mohamedfiras.mhalla@esprit.tn>
2022-07-25 00:02:03 +0100

    fix2 commit

--- src/test/java/tn/esprit/spring/ProjetApplicationTests.java
+++ src/test/java/tn/esprit/spring/ProjetApplicationTests.java
@@ -1,5 +1,6 @@
 package tn.esprit.spring;
 
+
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -8,6 +9,7 @@ class ProjetApplicationTests {
 
 	@Test
 	void contextLoads() {
+		System.out.println("Passed");
 	}
 
 }
